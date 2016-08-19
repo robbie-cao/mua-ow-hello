ece4b80f64638166608de17c7307863216c5f2c7
wyongfei <wyongfei@163.com>
2016-08-19 16:19:28 +0800

    add gpio isr demo
    Signed-off-by: wyongfei <wyongfei@163.com>

--- src/function_key.c
+++ src/function_key.c
@@ -2,22 +2,34 @@
 #include "unistd.h"
 #include "mraa.h"
 
+
+void isr(void *parm)
+{
+    printf("isr enter\n");
+}
+
 int
 main(int argc, char** argv)
 {
     mraa_init();
-    fprintf(stdout, "MRAA Version: %s\nStarting Read on IO6\n", mraa_get_version());
+    fprintf(stdout, "MRAA Version: %s\nStarting Read on IO16\n", mraa_get_version());
 
                 //! [Interesting]
     mraa_gpio_context gpio;
 
-    gpio = mraa_gpio_init(15);
+    gpio = mraa_gpio_init(16);
     mraa_gpio_dir(gpio, MRAA_GPIO_IN);
 
-    for (;;) {
-        fprintf(stdout, "Gpio is %d\n", mraa_gpio_read(gpio));
-        sleep(1);
-    }
+    //for (;;) {
+    //    fprintf(stdout, "Gpio is %d\n", mraa_gpio_read(gpio));
+    //    sleep(1);
+    //}i
+
+    printf("ISR TETS\r\n");
+    mraa_gpio_isr(gpio,MRAA_GPIO_EDGE_BOTH,&isr,NULL);
+    printf("..");
+
+    while(1);
 
     mraa_gpio_close(gpio);
             //! [Interesting]
