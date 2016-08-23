52a6fcb7318770f5c35a4a25c759ef425923c944
tqdong <tqd163@163.com>
2016-08-23 19:27:06 +0800

    Read data of LSM6DS3 register by i2c
    Signed-off-by: tqdong <tqd163@163.com>

--- src/function_key.c
+++ src/function_key.c
@@ -2,6 +2,8 @@
 #include "unistd.h"
 #include "mraa.h"
 
+#define GSENSOR_I2C_ADDR                                0x6A
+#define LSM6DS3_XG_WHO_AM_I_ADDR                        0x0F
 
 void isr(void *parm)
 {
@@ -11,10 +13,14 @@ void isr(void *parm)
 int
 main(int argc, char** argv)
 {
+    int res0 = 0;
+    int res1 = 0;
+
     mraa_init();
     fprintf(stdout, "MRAA Version: %s\nStarting Read on IO16\n", mraa_get_version());
 
                 //! [Interesting]
+#if 0
     mraa_gpio_context gpio;
 
     gpio = mraa_gpio_init(16);
@@ -33,6 +39,18 @@ main(int argc, char** argv)
 
     mraa_gpio_close(gpio);
             //! [Interesting]
+#endif
+
+    mraa_i2c_context i2c;
+    i2c = mraa_i2c_init(0);
+
+    fprintf(stdout, "i2c is %d\n", i2c);
+
+    res0 = mraa_i2c_address(i2c, GSENSOR_I2C_ADDR);
+    fprintf(stdout, "Set_addr is %d\n", res0);
+
+    res1 = mraa_i2c_read_byte_data(i2c, LSM6DS3_XG_WHO_AM_I_ADDR);
+    fprintf(stdout, "Read data is 0x%02x\n", res1);
 
     return 0;
 }
