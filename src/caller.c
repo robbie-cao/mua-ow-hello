9534647428f296934dd8289665435ff24ba09765
Robbie Cao <robbie.cao@gmail.com>
2016-08-31 16:29:19 +0800

    More simple caller without looper
    Signed-off-by: Robbie Cao <robbie.cao@gmail.com>

--- src/caller.c
+++ src/caller.c
@@ -6,44 +6,12 @@
 static struct ubus_context *ctx;
 static struct blob_buf b;
 
-static void caller_subscribe_cb(struct ubus_context *ctx, struct ubus_object *obj)
-{
-    fprintf(stderr, "Subscribers active: %d\n", obj->has_subscribers);
-}
-
-static struct ubus_object caller_object = {
-    .subscribe_cb = caller_subscribe_cb,
-};
-
-
-static void client_main(void)
-{
-   int ret;
-   uint32_t id;
-
-    ret = ubus_add_object(ctx, &caller_object);
-    if (ret) {
-        fprintf(stderr, "Failed to add_object object: %s\n", ubus_strerror(ret));
-        return;
-    }
-
-    if (ubus_lookup_id(ctx, "key", &id)) {
-        fprintf(stderr, "Failed to look up key object\n");
-        return;
-    }
-
-    ubus_invoke(ctx, id, "status", NULL, NULL, 0, 3000);
-
-    blob_buf_init(&b, 0);
-    blobmsg_add_u32(&b, "id", 1);
-    ubus_invoke(ctx, id, "get", b.head, NULL, 0, 3000);
-    blob_buf_free(&b);
-}
-
 int main(int argc, char **argv)
 {
     const char *ubus_socket = NULL;
     int ch;
+    int ret;
+    uint32_t id;
 
     while ((ch = getopt(argc, argv, "cs:")) != -1) {
         switch (ch) {
@@ -58,22 +26,25 @@ int main(int argc, char **argv)
     argc -= optind;
     argv += optind;
 
-    uloop_init();
-
     ctx = ubus_connect(ubus_socket);
     if (!ctx) {
         fprintf(stderr, "Failed to connect to ubus\n");
         return -1;
     }
 
-    ubus_add_uloop(ctx);
+    if (ubus_lookup_id(ctx, "key", &id)) {
+        fprintf(stderr, "Failed to look up key object\n");
+        return -1;
+    }
 
-    client_main();
+    ubus_invoke(ctx, id, "status", NULL, NULL, 0, 3000);
 
-    uloop_run();
+    blob_buf_init(&b, 0);
+    blobmsg_add_u32(&b, "id", 1);
+    ubus_invoke(ctx, id, "get", b.head, NULL, 0, 3000);
+    blob_buf_free(&b);
 
     ubus_free(ctx);
-    uloop_done();
 
     return 0;
 }
