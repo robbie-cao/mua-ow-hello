889fd64de62b1968a0e9870c5cf507b795778b9f
tqdong <tqd163@163.com>
2016-09-02 13:23:22 +0800

    Write data and read of pn532 by IIC
    Signed-off-by: tqdong <tqd163@163.com>

--- src/function_key.c
+++ src/function_key.c
@@ -3,6 +3,10 @@
 #include "mraa.h"
 #include "lsm6ds3.h"
 
+
+#define PN532_I2C_ADDR                                0x24
+
+
 void isr(void *parm)
 {
     printf("isr enter\n");
@@ -13,12 +17,13 @@ main(int argc, char** argv)
 {
     int res0 = 0;
     int res1 = 0;
-    uint16_t buf0[2] = {0x00, 0x00};
+    uint16_t buf0[10] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
     uint16_t buf1[2] = {0x00, 0x00};
     uint16_t buf2[2] = {0x00, 0x00};
     uint16_t buf3[2] = {0x00, 0x00};
     uint16_t buf4[2] = {0x00, 0x00};
     uint8_t buf5 = 0;
+    uint8_t i = 0;
 
     mraa_init();
     fprintf(stdout, "MRAA Version: %s\nStarting Read on IO16\n", mraa_get_version());
@@ -28,18 +33,19 @@ main(int argc, char** argv)
 
     fprintf(stdout, "i2c is %d\n", i2c);
 
-    mraa_i2c_address(i2c, GSENSOR_I2C_ADDR);
+    mraa_i2c_address(i2c, PN532_I2C_ADDR);
 
-    buf0[0] = 0x30;
+    buf0[0] = 0x00;
     buf0[1] = 0x00;
-    buf1[0] = 0x44;
-    buf1[1] = 0x00;
-    buf2[0] = 0x00;
-    buf2[1] = 0x00;
-    buf3[0] = 0x00;
-    buf3[1] = 0x00;
-    buf4[0] = 0x38;
-    buf4[1] = 0x00;
+    buf0[2] = 0xff;
+    buf0[3] = 0x02;
+    buf0[4] = 0xfe;
+    buf0[5] = 0xd4;
+    buf0[6] = 0x02;
+    buf0[7] = 0x2a;
+    buf0[8] = 0x00;
+    buf0[9] = 0x00;
+#if 0
     res0 = mraa_i2c_write_word_data(i2c, &buf0, LSM6DS3_CTRL1_XL);
     fprintf(stdout, "Set_addr_0 is %d - 0x%02x - 0x%02x\n", res0, buf0[0], buf0[1]);
     res0 = mraa_i2c_write_word_data(i2c, &buf1, LSM6DS3_CTRL3_C);
@@ -50,21 +56,21 @@ main(int argc, char** argv)
     fprintf(stdout, "Set_addr_0 is %d - 0x%02x - 0x%02x\n", res0, buf3[0], buf3[1]);
     res0 = mraa_i2c_write_word_data(i2c, &buf4, LSM6DS3_CTRL9_XL);
     fprintf(stdout, "Set_addr_0 is %d - 0x%02x - 0x%02x\n", res0, buf4[0], buf4[1]);
-
+#endif
     sleep(1);
-    res1 = mraa_i2c_read_byte_data(i2c, LSM6DS3_STATUS_REG);
-    fprintf(stdout, "Read data_1 is 0x%02x - 0x%02x\n", res1, LSM6DS3_STATUS_REG);
-    if (res1) {
-        res0 = mraa_i2c_read_byte_data(i2c, LSM6DS3_OUTX_L_XL);
-        fprintf(stdout, "Read data_1 is 0x%02x - 0x%02x\n", res0, LSM6DS3_OUTX_L_XL);
+    res0 = mraa_i2c_write(i2c, &buf0, 10);
+    fprintf(stdout, "Write data_0 is 0x%02x\n", res0);
+    for(i = 0; i < 10; i++) {
+        fprintf(stdout, "Write data_[%02x] is 0x%02x\n", i, buf0[i]);
     }
-    buf5 = 0x12;
-    res0 = mraa_i2c_write_byte_data(i2c, &buf5, LSM6DS3_FIFO_CTRL3);
-    fprintf(stdout, "Set_addr_0 is %d - 0x%02x\n", res0, buf5);
 
-    sleep(1);
-    res1 = mraa_i2c_read_byte_data(i2c, LSM6DS3_FIFO_CTRL3);
-    fprintf(stdout, "Read data_1 is 0x%02x\n", res1);
+//   sleep(1);
+    res1 = mraa_i2c_read(i2c, &buf0, 10);
+
+    fprintf(stdout, "Read data_0 is 0x%02x\n", res1);
+    for(i = 0; i < 10; i++) {
+        fprintf(stdout, "Read data_[%02x] is 0x%02x\n", i, buf0[i]);
+    }
 
     return 0;
 }
