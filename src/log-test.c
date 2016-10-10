35e2170139821be9743db42c0048595d52a39bb7
Robbie Cao <robbie.cao@gmail.com>
2016-10-10 15:09:30 +0800

    Test log level
    Signed-off-by: Robbie Cao <robbie.cao@gmail.com>

--- src/log-test.c
+++ src/log-test.c
@@ -7,12 +7,14 @@ int main(void)
 {
 	log_init();
 
+	log_set_level(LOG_LEVEL_ALL);
 	LOG_DEBUG("%d\n" , __LINE__);
 	LOG_INFO("%d\n"  , __LINE__);
 	LOG_WARN("%d\n"  , __LINE__);
 	LOG_ERROR("%d\n" , __LINE__);
 	LOG_FATAL("%d\n" , __LINE__);
 
+	log_set_level(LOG_LEVEL_WARN);
 	LOG_DEBUG("%s\n" , "Hello");
 	LOG_INFO("%s\n"  , "Hello");
 	LOG_WARN("%s\n"  , "Hello");
@@ -21,6 +23,15 @@ int main(void)
 
 	log_out(LOG_LEVEL_DEBUG, "%d, %s\n", __LINE__, __FUNCTION__);
 	D();
+
+	LOG_FATAL("%s\n" , __FILE__);
+
+	log_set_level(LOG_LEVEL_OFF);
+	LOG_DEBUG("%s\n" , __FILE__);
+	LOG_INFO("%s\n"  , __FILE__);
+	LOG_WARN("%s\n"  , __FILE__);
+	LOG_ERROR("%s\n" , __FILE__);
+	LOG_FATAL("%s\n" , __FILE__);
 
 	return 0;
 }
